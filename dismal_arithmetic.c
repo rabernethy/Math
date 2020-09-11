b2ea29c39961bb625483b4477e55e745e06a2011
Russell <rdaswimmer@gmail.com>
2020-09-11 00:20:36 -0400

    added dMult() and a few other things

--- dismal_arithmetic.c
+++ dismal_arithmetic.c
@@ -2,19 +2,20 @@
 
 int dAdd(int n1, int n2);
 int dMult(int n1, int n2);
+
 int reverse(int n);
 int max(int n1, int n2);
 int min(int n1, int n2);
 int nUnitPlaces(int n);
+int power(int base, int exponent);
 
 int main() {
 
-    int n1 = 0;
-    int n2 = 110024;
+    int n1 = 169;
+    int n2 = 248;
 
-
-    printf("There are %d unit palces in %d.\n", nUnitPlaces(n1), n1);
     printf("%d + %d = %d\n", n1, n2, dAdd(n1,n2));
+    printf("%d x %d = %d\n", n1, n2, dMult(n2, n1));
 }
 
 // returns the sum of two numbers following dismal arithmetic.
@@ -28,13 +29,29 @@ int dAdd(int n1, int n2) {
     return reverse(sum);
 }
 
+// returns the product of two numbers following dismal arithmetic.
 int dMult(int n1, int n2) {
-    int prod = 0, additionQueue[1];
+    int prod, additionQueue[nUnitPlaces(n1)], i, j;
+    int n1_up = nUnitPlaces(n1), n2_up = nUnitPlaces(n2);
+
+    //dismally multiply each unit in n1 with each unit in n2 and save the result.
+    for (i = 0; i < n1_up; i++) {
+        prod = 0;
+        int num2 = n2;
+
+        for (j = 0; j < n2_up; j++) {
+            prod = (prod * 10) + min(n1 % 10, num2 % 10);
+            num2 /= 10;
+        }
+        additionQueue[i] = reverse(prod) * power(10, i);
+        n1 /= 10;
+    }
     
-    
-    
-    
-    return 0;
+    // add all of the products together.
+    prod = 0;
+    for (i = 0; i < n1_up; i++)
+        prod = dAdd(prod, additionQueue[i]);
+    return prod;
 }
 
 // returns a lexigraphically reversed integer.
@@ -69,3 +86,12 @@ int nUnitPlaces(int n) {
     }
     return counter;
 }
+
+int power(int base, int exponent) {
+    if (exponent == 0)
+        return 1;
+    for (int i = 1; i < exponent; i++)
+        base *= base;
+    return base;
+    
+} 
